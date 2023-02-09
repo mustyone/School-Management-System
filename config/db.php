aef63da18b148fcf6c7bc32103c1abab7b6c2c96
Olukayode Adamolekun <vimkaf@gmail.com>
2023-02-09 18:39:31 +0100

    updated git ignore

--- config/db.php
+++ config/db.php
@@ -1,13 +1,2 @@
 <?php
-
-if(!defined('servername')) define("servername", "localhost");
-if(!defined('username')) define("username", "root");
-if(!defined('password')) define("password", "");
-if(!defined('database')) define("database", "school_management");
-
-
-$dbc = mysqli_connect(servername,username,password,database);
-if(!$dbc){
-    echo "Invalid database connection";
-    exit();
-}
+//copy db_sample.php
