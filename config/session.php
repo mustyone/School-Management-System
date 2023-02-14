12d778ad5897a75f5c0394b869855c490d168f7b
Olukayode Adamolekun <vimkaf@gmail.com>
2023-02-14 09:49:26 +0100

    Fixed session has been started error ignoring..

--- config/session.php
+++ config/session.php
@@ -1,1 +1,8 @@
-<?php session_start(); ?>
+<?php 
+
+//this will check if the session is not active before it starts a session
+//this should fix the warning that session has been started ignoring session_start
+if(session_status() !== PHP_SESSION_ACTIVE) session_start();
+
+
+?>
