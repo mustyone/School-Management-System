f9b8c3a5ea8e8ed08fcc4dd109dd88909a281b22
mustapha <mustaphaahmadmuhammadon@gmail.com>
2023-06-26 16:05:27 +0100

    corecting db migrations

--- migrations/db_migration_create_admission_application_form_2023_05_22.php
+++ migrations/db_migration_create_admission_application_form_2023_05_22.php
@@ -1,6 +1,6 @@
 <?php 
 function up(){
-    $query = "CREATE TABLE `school_management_system`.`admission_application_form` (`app_number` VARCHAR(100) NOT NULL , `fest_name` TEXT NOT NULL , `midle_name` TEXT NOT NULL , `last_name` TEXT NOT NULL , `address` TEXT NOT NULL , `date_of_birth` DATE NOT NULL , `nationality` VARCHAR(100) NOT NULL , `sex` VARCHAR(2) NOT NULL , `religion` VARCHAR(20) NOT NULL , `state_of_origin` VARCHAR(20) NOT NULL , `l_g_a` VARCHAR(50) NOT NULL ) ENGINE = InnoDB";
+    $query = "CREATE TABLE `school_management_system`.`admission_application_form` (`app_number` VARCHAR(100) NOT NULL , `first_name` TEXT NOT NULL , `middle_name` TEXT NOT NULL , `last_name` TEXT NOT NULL , `address` TEXT NOT NULL , `date_of_birth` DATE NOT NULL , `nationality` VARCHAR(100) NOT NULL , `sex` VARCHAR(2) NOT NULL , `religion` VARCHAR(20) NOT NULL , `state_of_origin` VARCHAR(20) NOT NULL , `l_g_a` VARCHAR(50) NOT NULL ) ENGINE = InnoDB";
 
     return $query;
 }
