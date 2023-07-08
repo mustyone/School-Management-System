10bddc48714d3591b3d02fdb908d808234182ad3
mustapha <mustaphaahmadmuhammadon@gmail.com>
2023-07-08 18:04:25 +0100

    modification of migarations on question_per_subject,mark_per_subject and adding class_id

--- migrations/db_migration_create_cbt_exams_2023_07_07.php
+++ migrations/db_migration_create_cbt_exams_2023_07_07.php
@@ -1,6 +1,6 @@
 <?php 
 function up(){
-    $query = "CREATE TABLE `school_management_system`.`cbt_exams` (`status` ENUM('active','inactive') NOT NULL , `question_ids` JSON NOT NULL , `exam_subject_id` JSON NOT NULL , `time_alloted_min` INT NOT NULL , `number_of_question_subject` INT NOT NULL , `allow_calculator` ENUM('yes','no') NOT NULL , `mark_per_question` DOUBLE NOT NULL , `overallscore` INT NOT NULL , `show_subject_per_question` ENUM('yes','no') NOT NULL , `show_result_after_submit` ENUM('yes','no') NOT NULL , `requre_id` ENUM('yes','no') NOT NULL ) ENGINE = InnoDB";
+    $query = "CREATE TABLE `school_management_system`.`cbt_exams` (`status` ENUM('active','inactive') NOT NULL , `question_ids` JSON NOT NULL , `exam_subject_id` JSON NOT NULL , `time_alloted_min` INT NOT NULL , `number_of_question_subject` JSON NOT NULL , `allow_calculator` ENUM('yes','no') NOT NULL , `mark_per_question` JSON NOT NULL , `overallscore` INT NOT NULL , `show_subject_per_question` ENUM('yes','no') NOT NULL , `show_result_after_submit` ENUM('yes','no') NOT NULL , `requre_id` ENUM('yes','no') NOT NULL,`class_id` INT NOT NULL ) ENGINE = InnoDB";
 
     return $query;
 }
